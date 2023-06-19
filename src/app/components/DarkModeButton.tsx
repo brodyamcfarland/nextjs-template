9f62718f41ed6d5e6a85dad525000cc6c1bc819d
Brody McFarland <brodyamcfarland@gmail.com>
2023-06-19 13:19:15 -0700

    Changing Spinning Animation on DarkMode Button to Desktop Only

--- src/app/components/DarkModeButton.tsx
+++ src/app/components/DarkModeButton.tsx
@@ -21,13 +21,13 @@ const DarkModeButton = () => {
                {theme === "dark" ? (
                     <TbSunFilled
                          size={25}
-                         className="opacity-70 hover:opacity-100 duration-300 hover:animate-[spin_3s_ease-in-out_infinite]"
+                         className="opacity-70 hover:opacity-100 duration-300 md:hover:animate-[spin_3s_ease-in-out_infinite]"
                          title="Light Mode"
                     />
                ) : (
                     <TbMoonFilled
                          size={25}
-                         className="opacity-70 hover:opacity-100 duration-300 hover:animate-[spin_3s_ease-in-out_infinite]"
+                         className="opacity-70 hover:opacity-100 duration-300 md:hover:animate-[spin_3s_ease-in-out_infinite]"
                          title="Dark Mode"
                     />
                )}
